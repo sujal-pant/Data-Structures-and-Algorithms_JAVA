5ecd2f3834fa7cd4ef90e5c3b9c968bacc74e613
sujal-pant <Sujapant77@gmail.com>
2024-03-24 00:53:44 +0545

    complete with note about variable, data type and more

--- variablesinputoutput.java
+++ variablesinputoutput.java
@@ -2,7 +2,53 @@ public class variablesinputoutput {
 
     public static void main(String[] args) {
         //this is my first step to the world of DATA STRUCTURES AND ALGORITHM
+
+        // Println means a new line after this and print means same as regular print of normal text
         System.out.println("hello world it my first step in the world of DSA");
-        
+        System.out.print("hello world");
+        // system is a class out is a object of print steam and println() is a method of print stream class
+        /*
+         *  file is compiled by the javac the compiler of java and then the source code is converted to
+         * the byte code and then the JVM convert the byte code into the binaryfile.
+         * java is a machine independened language
+         * 
+         * Java is a OOP language 
+         * Every thing in java is written in classes
+         * -->class is a user defined blueprint consisting of attribute and methods(fubction)
+         * Example:
+         * let us suppose a class named is "car" it has attribute as "
+         * company, color price and method as run () open trubk () and open window()
+         * 
+         * cars are made as audi , bmw and so on--> these are instances of class (objects)
+         * object --> instance of class
+         *  to know the color /--> BMW.color()
+         * 
+         * Know about Public static void main ( Strings args [])
+         * public --> access modifier
+         * void --> empty
+         * main --> function name ( entry point of java )
+         * static-->allows to call this method without creating a object of class
+         * string args [] -->[]--> list  agrs is a list / size can be any / 0th index (start from)
+         * 
+         */
+        System.out.println();
+        System.err.println();
+        System.out.println("JAVA VARIABLES");
+        System.out.println("title of reversed memory location");
+        // type variable_name = [value] -->syntax
+        int money=200;
+        System.out.println(money);
+        /* Rules for Naming variable
+         * 1.Whitespace are not allowed
+         * 2.keywords cant be used
+         * 3.Not start with Number
+        */
+       System.out.println();
+       System.out.println();
+       System.out.println("DATA TYPES IN DATA TYPES");
+       System.out.println("Primitive are built in , hold single value, cannot be divided ");
+       System.out.println("NON PRIMIVITE ARE user defined, memory address of a variable and  multi value");
+       System.out.println(" BOOLEAN,CHAR,BYTE,SHORT,INT,LONG,SHORT,FLOAT AND DOUBLE");
+
     }
 }
