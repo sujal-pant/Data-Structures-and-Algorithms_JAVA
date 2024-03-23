a3ef6b015fa96e8bfdc38eea8144c2d8c1f008c3
sujal-pant <Sujapant77@gmail.com>
2024-03-24 01:06:06 +0545

    taking input

---
+++ takinginput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class takinginput {
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        String  x = sc.nextLine();
+        System.out.println(x);
+
+
+    }
+}
+ /*
+  BY THE HELP OF sCANNER CLASS WE CAN DO INPUT IN JAVA  
+  ITS INSIDE OF util class
+  syntax:
+  Scanner sc = new Scanner (System.in)
+
+  */
+  
