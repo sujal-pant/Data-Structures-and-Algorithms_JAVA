d9cabd1838cc40be79ebd2d1b5a2a16b59cebb38
sujal-pant <Sujapant77@gmail.com>
2024-03-23 19:47:10 +0545

    renamed and enterd in the world of dsa

---
+++ variablesinputoutput.java
@@ -0,0 +1,8 @@
+public class variablesinputoutput {
+
+    public static void main(String[] args) {
+        //this is my first step to the world of DATA STRUCTURES AND ALGORITHM
+        System.out.println("hello world it my first step in the world of DSA");
+        
+    }
+}
