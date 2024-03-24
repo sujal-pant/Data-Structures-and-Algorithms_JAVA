c23c9cb1568bba5d4a0d23b44515f53f8fa9db86
sujal-pant <Sujapant77@gmail.com>
2024-03-24 14:27:17 +0545

    operators in java

---
+++ day3Operator.java
@@ -0,0 +1,39 @@
+public class day3Operator {
+    public static void main(String[] args) {
+        /*JAVA OPERATORS
+        1.Arthmetic operator
+        2.Relational Operator
+        3.Logical Operator
+        4.Assignment Operator
+        5.Uranary Operator
+        6.Bitwise operator
+        3+7= 10 --> 3 and 7 are operands and + is the operator
+
+         * 
+         */
+        System.out.println("Arthematic Operator");
+        System.out.println("eg : + - * / ");
+        System.out.println("Modulus % gives  the remainder");
+        System.out.println("increment or decrement operator");
+        int num = 10;
+        num++;
+        System.out.println(num);
+        System.out.println();
+        System.out.println("Relational Operator");
+        /*
+         * ==, ! ,<=. >= are some examples
+         */
+        System.out.println("logical operator");
+        /*
+         * &&, || ! are some example of logical operator
+         */
+        System.out.println("assignment operator");
+        /*
+         * =, +=, -=, /=, &= are some example pf assignment operator
+         */
+             
+         System.out.println();
+
+
+    }
+}
