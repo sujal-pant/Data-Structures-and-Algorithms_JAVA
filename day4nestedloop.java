21becf940e74a2e046c319fc5fcc4791ed6c2141
sujal-pant <Sujapant77@gmail.com>
2024-03-28 23:00:30 +0545

    updated with new codes added

--- day4nestedloop.java
+++ day4nestedloop.java
@@ -69,10 +69,108 @@ public class day4nestedloop {
        *   ****
        *   *****
        *   ******
+       * 
+       * 
+       * Total number of row =4
+       * empty spaces on each line is different
+       * r1->3 1
+       * r2->23
+       * r3-15
+       * r4-07
+       * (4-1) so on so the realtion on spaces are (r-i)
+       * and the star is (2*(i-r)
        */
-
-         
-
+      for (int i = 1; i <= 4; i++) {
+        // traversing over rows
+        // print (r-i) spaces
+        for (int j = 1; j <= 4 - i; j++) {
+            System.out.print(" ");
+        }
+        // print 2*i-1 stars
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            System.out.print("*");
+        }
+        // move to new line
+        System.out.println();
     }
+    
+    //numerical Rectangle Pattern
+    /*
+     * 1234567
+     * 2345671
+     * 3456712
+     * 4567123
+     * **so on
+     * 
+     */for(int i=1;i<=7;i++){
+        for(int j=i;j<=7;j++){
+            System.out.print(j);
+        }
+        for(int k=1;k<=i-1;k++){
+            System.out.print(k);
+        }
+        System.out.println();
+     }
+     System.out.println("end of this");
+     /*
+      * 123456
+        123456
+        123456
+        123456
+      */
+      for(int i=1;i<=6;i++){
+        for(int j=1;j<=6;j++){
+            System.out.print(j);
+        }
+        System.out.println();
+      }
+      System.out.println("end of this question");
+      /*
+       * 121212
+       * 212121
+       * 121212
+       * 212121
+       * i+j-->even are 1
+       * i+j-->odd are 2
+       */
+      for(int i=1;i<=6;i++){
+        for(int j=1;j<=6;j++){
+           if((i+j)%2==0){
+            System.err.print("1");
+           }
+           else{
+            System.out.print("2");
+           }
+        }
+        System.out.println();
 
-}
+    
+    }
+    /*
+     * 1
+     *121
+     12321
+     123421
+
+     r-i spaces
+     r-i 1to i and i-1 to 1
+     */
+    
+    for (int i = 1; i <= 4; i++) {
+    // Print (4-i) spaces
+    for (int j = 1; j <= 4 - i; j++) {
+        System.out.print(" ");
+    }
+    
+    // Print numbers from 1 to i
+    for (int k = 1; k <= i; k++) {
+        System.out.print(k);
+    }
+    
+    // Print numbers from (i-1) to 1
+    for (int l = i - 1; l >= 1; l--) {
+        System.out.print(l);
+    }
+    
+    System.out.println();
+    }}}
