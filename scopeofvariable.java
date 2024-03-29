c235a68c05898de663d0ccd0053057b6dd0665f0
sujal-pant <Sujapant77@gmail.com>
2024-03-29 21:53:56 +0545

    scopes of java

---
+++ scopeofvariable.java
@@ -0,0 +1,23 @@
+public class scopeofvariable {
+    public static void main(String[] args) {
+        
+        /*region from where a variable can be accessed.
+        1.class , block method level scope
+         * formal parameter and actual parameter
+         * formal para-> defined during function defination
+         * int sum ( int a, int b)--<formal 
+         * sum(a,b)--> while calling actual parameter(arguments)
+         * PASS BY VALUE AND PASS BY REFRENCE
+         * PBV--> copy the value of actual parameter
+         * called function its own copies
+         * PSR-> pass the parameter as the refrences (address)
+         *called function does not create its own copy 
+         *
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+    }
+}
